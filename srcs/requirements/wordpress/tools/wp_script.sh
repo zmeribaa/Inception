fb60973884e9fc7857f157dd55b8f1103a28318f
ziyad <ziyad@ziyads-MacBook-Pro.local>
2022-10-18 22:59:08 +0100

    git wp

---
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [! -f /var/html/wp-config.php]; then
+	mkdir -p /var/www/html
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar;
+	mv wp-cli.phar /usr/local/bin/wp;
+	cd /var/www/html;
+	wp core download --allow-root;
+	mv /var/www/wp-config.php /var/www/html/
+	wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL};
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+fi
+
+exec "$@"
