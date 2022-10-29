0a59e5c38362366d08a8120a6f59a8397947b5ca
ziyad <ziyad@ziyads-MacBook-Pro.local>
2022-10-29 16:07:32 +0100

    css fix

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -83,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('CONCATENATE_SCRIPTS', false);
 
 /* That's all, stop editing! Happy publishing. */
 
